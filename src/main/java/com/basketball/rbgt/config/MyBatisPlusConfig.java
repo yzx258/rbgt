8a8136c623c5ac3094eab7550ef5a689f2c2e5f8
yucw@itsmycar.cn <123456>
2020-09-09 16:14:25 +0800

    新增性能插件

--- src/main/java/com/basketball/rbgt/config/MyBatisPlusConfig.java
+++ src/main/java/com/basketball/rbgt/config/MyBatisPlusConfig.java
@@ -3,24 +3,40 @@ package com.basketball.rbgt.config;
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.aop.interceptor.PerformanceMonitorInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * @author yiautos
+ */
 @MapperScan("com.basketball.rbgt.mapper")
 @EnableTransactionManagement
-@Configuration//配置类
+@Configuration
 public class MyBatisPlusConfig {
-    //注册乐观锁插件
+    /**
+     * 注册乐观锁插件
+     */
     @Bean
     public OptimisticLockerInterceptor optimisticLockerInterceptor() {
         return new OptimisticLockerInterceptor();
     }
 
-    //分页插件
+    /**
+     * 分页插件
+     */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
     }
+
+//    /**
+//     * 性能分析插件
+//     */
+//    @Bean
+//    public PerformanceMonitorInterceptor performanceMonitorInterceptor() {
+//        return new PerformanceMonitorInterceptor();
+//    }
 
 }
